fcb73b40845973390c3f01c90d25607ed04a4430
kwijik <joebalalaika@ya.ru>
2019-02-01 10:06:21 +0100

    fix errors

--- app.cpp
+++ app.cpp
@@ -5,13 +5,23 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <string>
 #include <functional>
+#include <QApplication>
 
 using namespace cv;
 
+void drawTriangle(Mat image, Point p1, Point p2, Point p3, Scalar sc, int width){
+	line( image, p1, p2, sc, width, 8 );
+	line( image, p1, p3, sc, width, 8 );
+	line( image, p2, p3, sc, width, 8 );
+}
+
+// gnome & kde
+
+//
 
 int main(){
 
-
+	const int line_width = 2; 
 
 	int size_pic = 400;
 
@@ -23,7 +33,7 @@ int main(){
 
 	int img_size = 400;
 
-	Mat image = Mat::zeros( 400, 400, CV_8UC3 );
+	Mat image ( 400, 400, CV_8UC3, Scalar(255,255,0) ); // unsigned double
 
 
 
@@ -31,7 +41,7 @@ int main(){
 	for (int i=0; i < 4; i++){
 		for (int j=0; j<4; j++){
 			int fig = h & 3; // 3 это 11  
-			Scalar sc;
+			Scalar sc; // цвет
  			h >>= 2; // смещаем вправо на 2 бита
 
  			int col = h & 3; // 3 это 11  
@@ -39,21 +49,36 @@ int main(){
  			h >>= 2; // смещаем вправо на 2 бита
 
  			switch(col){
- 				case 0: sc = (255, 255, 255);
-		 		case 1: sc = (0, 0, 0);
-		 		case 2: sc = (255, 0, 0);
-		 		case 3: sc = (0, 0, 255);
+ 				case 0: 
+ 					sc = Scalar(255, 255, 255);
+ 					break;
+		 		case 1: 
+		 			sc = Scalar(0, 0, 0);
+		 			break;
+		 		case 2: 
+		 			sc = Scalar(255, 0, 0);
+		 			break;
+		 		case 3: 
+		 			sc = Scalar(0, 0, 255);
+		 			break;
  			}
 
-
 			switch(fig){
-				case 0: circle( image, Point( 200, 200 ), 32.0, sc, 1, 8 );
-				case 1: line( image, Point( i * 100, j * 100 ), Point( i * 100 +100, j * 100 +100), sc, 1, 8 );
-				case 2: line( image, Point( i*100, j * 100 + 100 ), Point( i * 100 +100, j * 100), sc, 1, 8 );
-				case 3: line( image, Point( i*100, j * 100 + 100 ), Point( i * 100 +100, j * 100), sc, 1, 8 );
+				case 0: 
+					circle( image, Point( i * 100 + 50, j * 100 + 50 ), 32.0, sc, line_width, LINE_8 );
+					break;
+				case 1: 
+					line( image, Point( i * 100, j * 100 ), Point( i * 100 +100, j * 100 +100), sc, line_width, LINE_8 );
+					break;
+				case 2: 
+					line( image, Point( i*100, j * 100 + 100 ), Point( i * 100 +100, j * 100), sc, line_width, LINE_8 );
+					break;
+				case 3: 
+					drawTriangle( image, Point( i*100, j * 100), Point( i * 100 +100, j * 100), Point(i * 100 +50, j * 100 + 100), sc, line_width);
+					break;
 			}
 
-		std::cout << "fig is " << fig << std::endl;
+			std::cout << "color is " << col << "; fig is " << fig <<  std::endl;
 		}
 	}
 /*
