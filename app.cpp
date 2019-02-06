ee04ca9f834ef4f952ee5b49f7e0c6d2dd1a1831
kwijik <joebalalaika@ya.ru>
2019-02-06 19:11:31 +0100

    cleaning code

--- app.cpp
+++ app.cpp
@@ -5,7 +5,6 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <string>
 #include <functional>
-#include <QApplication>
 
 using namespace cv;
 
@@ -81,46 +80,6 @@ int main(){
 			std::cout << "color is " << col << "; fig is " << fig <<  std::endl;
 		}
 	}
-/*
- 	int fig = h & 3; // 3 это 11  == 0
-
- 	std::cout << "fig: " << fig << std::endl; 
-
-
- 	h >>= 2; // смещаем вправо на 2 бита
-
-	std::cout << "h: " << h << std::endl;
-
-
- 	 int col = h & 3; // 3 это 11  
-
-	std::cout << "col: " << col << std::endl;
-
- 	h >>= 2; // смещаем влево на 2 бита
-
- 	int test1 = 4;
- 	std::cout << "test1: " << test1 << std::endl;
-
- 	test1 >>= 2;
- 	std::cout << "test1: " << test1 << std::endl;
-
- 	int test2 = 5 & 3;
- 	std::cout << "test2: " << test2 << std::endl;
-
-/*
- 	switch(temp){
- 		case 0: // рисуем круг
- 		case 1: // рисуем цвет
- 	} 
-
-	Mat image = Mat::zeros( 400, 400, CV_8UC3 );
-
-	line( image, Point( 15, 20 ), Point( 70, 50), Scalar( 110, 220, 0 ),  2, 8 );
-
-	  	circle( image, Point( 200, 200 ), 32.0, Scalar( 110, 220, 0 ), 1, 8 );
-
-
- 	*/
 
 
 
