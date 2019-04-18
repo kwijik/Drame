0c2fbd4bc6450df8928b89bfd561c29b0155a43a
kwijik <joebalalaika@ya.ru>
2019-04-18 21:05:36 +0200

    qt test
    add opencv ~test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPixmap>
+#include <QMessageBox>
+#include <iostream>
+#include <string>
+#include <functional>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+
+using namespace cv;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,3 +26,79 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void generator(){
+
+	const int line_width = 2; 
+
+	int size_pic = 400;
+
+	std::string test = "tttt";
+
+	std::size_t h = std::hash <std::string>{}(test); // hash
+
+	std::cout << "h: " << h << std::endl;
+
+	int img_size = 400;
+
+	Mat image ( 400, 400, CV_8UC3, Scalar(255,255,0) ); // unsigned double
+
+
+
+	int fig = h & 3; // 3 это 11  
+	std::cout << "fig is " << fig <<  std::endl;
+
+ 	h >>= 2; // смещаем вправо на 2 бита
+	std::cout << "h is " << h <<  std::endl;
+
+ 	int col = h & 3; // 3 это 11  
+	std::cout << "col is " << col <<  std::endl;
+
+ 	h >>= 2; // смещаем вправо на 2 бита
+
+// 
+	for (int i=0; i < 4; i++){
+		for (int j=0; j<4; j++){
+			int fig = h & 3; // 3 это 11  
+
+			Scalar sc; // color
+ 			h >>= 2; 
+
+ 			int col = h & 3; 
+
+ 			h >>= 2; 
+
+ 			switch(col){
+ 				case 0: 
+ 					sc = Scalar(255, 255, 255);
+ 					break;
+		 		case 1: 
+		 			sc = Scalar(0, 0, 0);
+		 			break;
+		 		case 2: 
+		 			sc = Scalar(255, 0, 0);
+		 			break;
+		 		case 3: 
+		 			sc = Scalar(0, 0, 255);
+		 			break;
+ 			}
+
+			switch(fig){
+				case 0: 
+					circle( image, Point( i * 100 + 50, j * 100 + 50 ), 32.0, sc, line_width, LINE_8 );
+					break;
+				case 1: 
+					line( image, Point( i * 100, j * 100 ), Point( i * 100 +100, j * 100 +100), sc, line_width, LINE_8 );
+					break;
+				case 2: 
+					line( image, Point( i*100, j * 100 + 100 ), Point( i * 100 +100, j * 100), sc, line_width, LINE_8 );
+					break;
+				case 3: 
+					drawTriangle( image, Point( i*100, j * 100), Point( i * 100 +100, j * 100), Point(i * 100 +50, j * 100 + 100), sc, line_width);
+					break;
+			}
+
+			std::cout << "color is " << col << "; fig is " << fig <<  std::endl;
+		}
+
+	}
