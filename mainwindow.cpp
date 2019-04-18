adfd8481846c0565bc394e9ac1a6ff5be8287171
kwijik <joebalalaika@ya.ru>
2019-04-19 01:04:35 +0200

    fixed empty string problem

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,44 +23,38 @@ QImage generator( std::string text){
 
     const int line_width = 2;
 
-    int size_pic = 400;
 
-    //std::string test = "tttt";
-
-    std::size_t h = std::hash <std::string>{}(text); // hash
+    std::size_t h = std::hash <std::string>{}(text); 
 
     std::cout << "h: " << h << std::endl;
 
     int img_size = 400;
 
-    Mat image ( 400, 400, CV_8UC3, Scalar(255,255,0) ); // unsigned double
+    Mat image ( img_size, img_size, CV_8UC3, Scalar(255,255,0) ); 
 
 
-    //int test1 = 10 >>2;
-    //std::cout << "test1 is " << test1 <<  std::endl;
 
-    int fig = h & 3; // 3 это 11; берем 2 бита
+    int fig = h & 3; 
     std::cout << "fig is " << fig <<  std::endl;
 
-    h >>= 2; // смещаем вправо на 2 бита
+    h >>= 2; 
     std::cout << "h is " << h <<  std::endl;
 
-    int col = h & 3; // 3 это 11
+    int col = h & 3; 
     std::cout << "col is " << col <<  std::endl;
 
-    h >>= 2; // смещаем вправо на 2 бита
+    h >>= 2; 
 
-//
     for (int i=0; i < 4; i++){
         for (int j=0; j<4; j++){
-            int fig = h & 3; // 3 это 11
+            int fig = h & 3; 
 
-            Scalar sc; // цвет
-            h >>= 2; // смещаем вправо на 2 бита
+            Scalar sc; 
+            h >>= 2; 
 
-            int col = h & 3; // 3 это 11
+            int col = h & 3; 
 
-            h >>= 2; // смещаем вправо на 2 бита
+            h >>= 2; 
 
             switch(col){
                 case 0:
@@ -92,16 +86,8 @@ QImage generator( std::string text){
                     break;
             }
 
-            // std::cout << "color is " << col << "; fig is " << fig <<  std::endl;
         }
     }
-
-
-
-   // imshow("Image",image);
-
-    // waitKey(0);
-//QImage imgIn= QImage((uchar*) img.data, img.cols, img.rows, img.step, QImage::Format_RGB888);
 
     return QImage((uchar*) image.data, image.cols, image.rows, image.step, QImage::Format_RGB888);
 }
@@ -111,7 +97,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //QPixmap pix("/Users/denisbolshakov/Documents/cpp/DrawMe/2019-02-03 02.45.44.jpg");
     QLineEdit* lineEdit = this->findChild<QLineEdit*>("lineEdit");
     connect(lineEdit, SIGNAL(textChanged(const QString &)), this, SLOT(updateImage(const QString &)));
 
@@ -121,24 +106,23 @@ MainWindow::~MainWindow()
 {
      delete ui;
 }
-
 void MainWindow::updateImage(const QString &str)
 {
-   // QString str = ui->lineEdit->text();
-    //std::cout << str << std::endl;
-    // generator(str.toUtf8().constData()).save("test_image.png");
     std::cout << "string is: " << str.toUtf8().constData() << std::endl;
-    QPixmap pix(QPixmap::fromImage(generator(str.toUtf8().constData())));
 
-    int w = ui->labelPic->width();
-    int h = ui->labelPic->height();
-    std::cout << "setPixmap goes here..." << std::endl;
+    if (str.isEmpty()){
+        ui->labelPic->clear();
+    } else {
+        QPixmap pix(QPixmap::fromImage(generator(str.toUtf8().constData())));
 
-    ui->labelPic->setPixmap(pix.scaled(w,h, Qt::KeepAspectRatio));
+        int w = ui->labelPic->width();
+        int h = ui->labelPic->height();
+        std::cout << "setPixmap goes here..." << std::endl;
+
+        ui->labelPic->setPixmap(pix.scaled(w,h, Qt::KeepAspectRatio));
+    }
     std::cout << "update goes here..." << std::endl;
      QWidget::update();
     // QWidget::repaint();
      std::cout << "after update goes here..." << std::endl;
-
-     //
 }
